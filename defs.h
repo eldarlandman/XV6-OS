7b3e55fea96553d0e0113c0415d987a5a923d980
gal <galtfilin@gmail.com>
2016-03-20 16:07:23 +0200

    added wait2 system call. the concreate wait2 implementation is not whole. the code was not tested yet.

--- defs.h
+++ defs.h
@@ -118,6 +118,8 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+int wait2(int *retime, int *rutime, int *stime);
+
 
 // swtch.S
 void            swtch(struct context**, struct context*);
--- user.h
+++ user.h
@@ -24,6 +24,8 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 
+int wait2(int *, int *, int *);
+
 // ulib.c
 int stat(char*, struct stat*);
 char* strcpy(char*, char*);
