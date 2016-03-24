646a90eb990675ac7e355a7cd0efc627ca20a9d6
eldarlandman <eldarlandman@gmail.com>
2016-03-24 03:29:43 +0200

    finished sanity tests. started smlSanity

--- sanity.c
+++ sanity.c
@@ -6,20 +6,22 @@
 #define S_CPU 1
 #define IO 2
 
+#define STIME 0  //sleeping or IO time
+#define RETIME 1 //ready or waiting time
+#define  RUTIME 2 //running time
+#define TOTAL 3 //total processes per type
+
 
 int
 main(int argc, char *argv[])
 {
   char * type;
   int pid, n, i, j, k;
-  int stime = 0;
-  int retime = 0;
-  int rutime = 0;
-  /*
-int stime_acc=0;
-int retime_acc=0;
-int rutime_acc=0;
-*/
+  
+  int retime, stime, rutime;
+  
+int statis [3][4]; 
+
   if (argc == 2)
   {
     n = atoi(argv[1]);
@@ -56,19 +58,34 @@ int rutime_acc=0;
     }
     for (i = 0; i < n * 3; i++)
     {
+      
       pid= wait2(&retime, &rutime, &stime);
       switch(pid % 3)
       {
 	case(CPU):
 	  type = "CPU";
-	  
+	  statis[CPU][STIME]+=stime;
+	  statis[CPU][RETIME]+=retime;
+	  statis[CPU][RUTIME]+=rutime;
+	  statis[CPU][TOTAL]++;  
 	  break;
+	  
 	case(S_CPU):
 	  type = "S-CPU";
+	  statis[S_CPU][STIME]+=stime;
+	  statis[S_CPU][RETIME]+=retime;
+	  statis[S_CPU][RUTIME]+=rutime;
+	  statis[S_CPU][TOTAL]++;  
 	  break;
+	  
 	case(IO):
-	  type = "IO";
+	  type = "IO";	  
+	  statis[IO][STIME]+=stime;
+	  statis[IO][RETIME]+=retime;
+	  statis[IO][RUTIME]+=rutime;
+	  statis[IO][TOTAL]++;
 	  break;
+	  
 	default:
 	  type = "this should not happen";
 	  break;
@@ -77,11 +94,17 @@ int rutime_acc=0;
 	      pid, type, retime, rutime, stime);
     }
   
-  /*
-  printf(1, "average CPU time= %d  || average S-CPU time= %d || average IO time= %d\n,
-  )*/
+  
+  printf(1, "average time for CPU processes:    sleeping  time= %d || ready time=%d ||turnaround time= %d \n",
+  statis[CPU][STIME] / statis[CPU][TOTAL]  ,  statis[CPU][RETIME] / statis[CPU][TOTAL]  ,  (statis[CPU][STIME] +statis[CPU][RETIME]+ statis[CPU][RUTIME] ) / statis[CPU][TOTAL]  );
     
-    
+printf(1, "average time for S_CPU processes:    sleeping  time= %d || ready time=%d ||turnaround time= %d \n",
+  statis[S_CPU][STIME] / statis[S_CPU][TOTAL]  ,  statis[S_CPU][RETIME] / statis[S_CPU][TOTAL]  ,  (statis[S_CPU][STIME] +statis[S_CPU][RETIME]+ statis[S_CPU][RUTIME] ) / statis[S_CPU][TOTAL]  );
+  
+printf(1, "average time for IO processes:    sleeping  time= %d || ready time=%d ||turnaround time= %d \n",
+  statis[IO][STIME] / statis[IO][TOTAL]  ,  statis[IO][RETIME] / statis[IO][TOTAL]  ,  (statis[IO][STIME] +statis[IO][RETIME]+ statis[IO][RUTIME] )/ statis[IO][TOTAL]  );
+  
+  
   }
   else
   {
