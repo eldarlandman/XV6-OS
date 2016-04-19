724b24d5bef3e2662435798489f75001172fefd2
galtfilin <galtfilin@gmail.com>
2016-04-20 01:36:27 +0300

    better test file

--- test.c
+++ test.c
@@ -3,29 +3,36 @@
 #include "fcntl.h"
 
 void myHandler(int pid, int value)
-{  
+{
  printf(2, "%d sent %d\n", pid, value); 
 }
 
 int
 main(void)
 {
-  int i;
+  int i, j;
   sigset(myHandler);
   int pid = fork();
   if (pid != 0)
   {
-    /*fork();
+    for (i = 0; i < 100000; ) {i++;}
     fork();
-    fork();*/
     fork();
-    
-      sigsend(pid, 20);
+    fork();
+    fork();
+    fork();
+    sigsend(pid, 20);
   }
-  
-  for (i = 0; i < 100000; ) {i++;}
-  for (i = 0; i < 8; i++)
-    wait();
+  else
+  {
+    for (j = 0; j < 1000; j++)
+      for (i = 0; i < 10000; ) {i++;}
+  }
+  if (pid != 0)
+  {
+    for (i = 0; i < 33; i++)
+      wait();
+  }
   
   exit();
 }
