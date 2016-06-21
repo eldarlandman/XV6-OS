c6c0785604c348841a98b05e2cbce3da01964c7d
eldarlandman <eldarlandman@gmail.com>
2016-06-21 19:36:50 +0300

    task 4 works. all user tests passed!

--- mkfs.c
+++ mkfs.c
@@ -19,6 +19,7 @@
 #endif
 
 #define NINODES 200
+#define KERSIZE 180570
 
 // Disk layout:
 // [ boot block | sb block | log | inode blocks | free bit map | data blocks ]
@@ -40,6 +41,7 @@ uint freeblock;
 
 void balloc(int);
 void wsect(uint, void*);
+void wsectAbs(uint, void*);
 void winode(uint, struct dinode*);
 void rinode(uint inum, struct dinode *ip);
 void rsect(uint sec, void *buf);
@@ -71,40 +73,49 @@ xint(uint x)
 
 int
 main(int argc, char *argv[])
-{
+{  
   int i, cc, fd, bootblockFD;
   uint rootino, inum, off;
   struct dirent de;
   char buf[BSIZE];
   struct dinode din;
+  int kernelCodeBlockskSize;
+  
+  int kernelFD = open(argv[3], O_RDWR);
+  kernelCodeBlockskSize= (   (lseek(kernelFD, 0, SEEK_END) +1)/BSIZE  )+1;
+  lseek(kernelFD, 0, SEEK_SET);
+  
+  printf("\n kernelSize: %d \n", kernelCodeBlockskSize);
   
   for (i = 0; i < 4; i++)//set the partitions offsets for future use
   {
-    partitionOffsets[i] = 1 + (PART_SIZE * i);
+    partitionOffsets[i] = 1+kernelCodeBlockskSize + (PART_SIZE * i);  //TODO consider remove 1+
   }
-
+  
   static_assert(sizeof(int) == 4, "Integers must be 4 bytes!");
-
+  
   if(argc < 4){
     fprintf(stderr, "Usage: mkfs fs.img files...\n");
     exit(1);
   }
-
+  
   assert((BSIZE % sizeof(struct dinode)) == 0);
   assert((BSIZE % sizeof(struct dirent)) == 0);
-
+  
   fsfd = open(argv[1], O_RDWR|O_CREAT|O_TRUNC, 0666);
   if(fsfd < 0){
     perror(argv[1]);
     exit(1);
   }
-
-    if (strcmp(argv[2], "bootblock") != 0)
+  
+  if (strcmp(argv[2], "bootblock") != 0)
   {
     fprintf(stderr, "Usage: mkfs fs.img bootblock kernel files...\n");
     exit(1);
   }
+  
 
+  /*-------------------the next code lines initlize super block metadata ONLY FOR THE FIRST PARTITION-------------------*/
   
   // 1 fs block = 1 disk sector
   nmeta = 1 + nlog + ninodeblocks + nbitmap;		//gal: partition offset + 1 block for super block
@@ -120,66 +131,109 @@ main(int argc, char *argv[])
   sb.partitionNumber = 0;	//the root partition is partition 0
   
   printf("nmeta %d (boot, super, log blocks %u inode blocks %u, bitmap blocks %u) blocks %d total %d\n",
-         nmeta, nlog, ninodeblocks, nbitmap, nblocks, PART_SIZE);
-
+	 nmeta, nlog, ninodeblocks, nbitmap, nblocks, PART_SIZE);
+  
   freeblock = nmeta;     // the first free block that we can allocate
 
-  for(i = -1; i < FSSIZE - 1; i++)
-    wsect(i, zeroes);
+/*-------------------finish  initlize super block metadata ONLY FOR THE FIRST PARTITION--------------------------------------*/  
+
   
-    bootblockFD = open(argv[2], O_RDWR);
+  
+/*-------------------copying to fs.img the mbr- table_partitions(type, offset, size, flags) , bootloader, magic nums   - to block 0-------------------*/ 
+
+for(i = 0; i < FSSIZE ; i++) //nullify all fs.img blocks
+    wsectAbs(i, zeroes);
+ 
+  bootblockFD = open(argv[2], O_RDWR);
   if (bootblockFD < 0)
   {
     perror(argv[2]);
     exit(1);
   }
-
+  
   
   memset(buf, 0, sizeof(buf));					//gal: nullify buf: a buffer the size of a block allocated on the stack
   struct mbr newMbr;						//allocate mbr struct on the stack
   memset(newMbr.bootstrap, 0, BOOTSTRAP);	//nullify the bootstrap TODO consider removal, for some reason when this was allocated it was filled with junk
-    if (read(bootblockFD, &newMbr, BOOTSTRAP) < BOOTSTRAP)
+  if (read(bootblockFD, &newMbr, BOOTSTRAP) < BOOTSTRAP)
   {
     perror(argv[2]);
     exit(1);
   }
-
+  
   close(bootblockFD);
   
   for (i = 0; i < 4; i++)
   {
-      newMbr.partitions[i].type = FS_INODE;
-      newMbr.partitions[i].offset = partitionOffsets[i];				//partition 0 starts from block 1
-      newMbr.partitions[i].size = PART_SIZE;			//partition 0 size is FSSIZE = 1000 block
-      newMbr.partitions[i].flags = 0;
+    newMbr.partitions[i].type = FS_INODE;
+    newMbr.partitions[i].offset = partitionOffsets[i];				//partition 0 starts from block 1
+    newMbr.partitions[i].size = PART_SIZE;			//partition 0 size is FSSIZE = 1000 block
+    newMbr.partitions[i].flags = 0;
   }
   newMbr.partitions[0].flags = PART_ALLOCATED | PART_BOOTABLE; //set partition 0 to be bootable and allocated. 
   newMbr.magic[0] = 0x55;
   newMbr.magic[1] = 0xAA;
   
   memmove(buf, &newMbr, sizeof(newMbr));		//copy the super block into the allocated buffer
-  wsect(-1, buf);								//write the buffer(mbr) into the first block
+  wsectAbs(0, buf);								//write the buffer(mbr) into the first block
+  
+/*-------------------finish copying mbr to block 0-------------------*/
+  
+	
+	
+/*-------------------the next code is copying kernel code to fs.img right after bootloader  code(block 0)-------------------*/
+      
+  
+  int readed;
+  if (kernelFD < 0)
+  {
+    perror(argv[2]);
+    exit(1);
+  }  
+  
+  //kernelCodeBlockskSize+1
+  for (i =1; i<kernelCodeBlockskSize;  i++){
+    
+    memset(buf, 0, sizeof(buf));	//nullify buf: a buffer the size of a block allocated on the stack    
+    
+    readed=read(kernelFD, &buf, BSIZE);
+    if (readed < 0)
+    {     
+      perror(argv[3]);
+      exit(1);
+    }
+     
+    wsectAbs(i, buf);	//write the buffer(block of kernel code) into the first block
+    
+  }
+  close(bootblockFD); 
+   
+/* -------------------finish copying kernel code-------------------*/
+  
 
+/*-------------------the next code is copying the user  files into fs.img on the first partition-------------------*/
   memset(buf, 0, sizeof(buf));		//gal: nullify buf: a buffer the size of a block allocated on the stack
   memmove(buf, &sb, sizeof(sb));	//copy the super block into the allocated buffer
   wsect(0, buf);					//write the buffer(super block) into the first block
-
+  
   rootino = ialloc(T_DIR);
   assert(rootino == ROOTINO);//shold pass for every partition, the inode num is relative to the partition
-
+  
   bzero(&de, sizeof(de));
   de.inum = xshort(rootino);
   strcpy(de.name, ".");
   iappend(rootino, &de, sizeof(de));
-
+  
   bzero(&de, sizeof(de));
   de.inum = xshort(rootino);
   strcpy(de.name, "..");
   iappend(rootino, &de, sizeof(de));
-
+  
+  
   for(i = 4; i < argc; i++){
+    
     assert(index(argv[i], '/') == 0);
-
+    
     if((fd = open(argv[i], 0)) < 0){
       perror(argv[i]);
       exit(1);
@@ -191,36 +245,50 @@ main(int argc, char *argv[])
     // in place of system binaries like rm and cat.
     if(argv[i][0] == '_')
       ++argv[i];
-
+    
     inum = ialloc(T_FILE);
-
+    
     bzero(&de, sizeof(de));
     de.inum = xshort(inum);
     strncpy(de.name, argv[i], DIRSIZ);
     iappend(rootino, &de, sizeof(de));
-
+    
     while((cc = read(fd, buf, sizeof(buf))) > 0)
       iappend(inum, buf, cc);
-
+    
     close(fd);
   }
-
+  
   // fix size of root inode dir
   rinode(rootino, &din);
   off = xint(din.size);
   off = ((off/BSIZE) + 1) * BSIZE;
   din.size = xint(off);
   winode(rootino, &din);
-
+/* -------------------finish copying user files-------------------*/
+  
   balloc(freeblock);
-
+  
   exit(0);
 }
 
 void
+wsectAbs(uint sec, void *buf){
+  
+  if(lseek(fsfd, sec * BSIZE, 0) != sec * BSIZE){
+    perror("lseek");
+    exit(1);
+  }
+  if(write(fsfd, buf, BSIZE) != BSIZE){
+    perror("write");
+    exit(1);
+  }
+  
+}
+void
 wsect(uint sec, void *buf)
-{
-  sec = sec + partitionOffsets[sb.partitionNumber];
+{  
+  sec = sec + partitionOffsets[sb.partitionNumber];  
   if(lseek(fsfd, sec * BSIZE, 0) != sec * BSIZE){
     perror("lseek");
     exit(1);
@@ -237,7 +305,7 @@ winode(uint inum, struct dinode *ip)
   char buf[BSIZE];
   uint bn;
   struct dinode *dip;
-
+  
   bn = IBLOCK(inum, sb);
   rsect(bn, buf);
   dip = ((struct dinode*)buf) + (inum % IPB);
@@ -251,7 +319,7 @@ rinode(uint inum, struct dinode *ip)
   char buf[BSIZE];
   uint bn;
   struct dinode *dip;
-
+  
   bn = IBLOCK(inum, sb);
   rsect(bn, buf);
   dip = ((struct dinode*)buf) + (inum % IPB);
@@ -267,6 +335,7 @@ rsect(uint sec, void *buf)
     exit(1);
   }
   if(read(fsfd, buf, BSIZE) != BSIZE){
+    close(fsfd);
     perror("read");
     exit(1);
   }
@@ -277,7 +346,7 @@ ialloc(ushort type)
 {
   uint inum = freeinode++;
   struct dinode din;
-
+  
   bzero(&din, sizeof(din));
   din.type = xshort(type);
   din.nlink = xshort(1);
@@ -291,7 +360,7 @@ balloc(int used)
 {
   uchar buf[BSIZE];
   int i;
-
+  
   printf("balloc: first %d blocks have been allocated\n", used);
   assert(used < BSIZE*8);
   bzero(buf, BSIZE);
@@ -313,7 +382,7 @@ iappend(uint inum, void *xp, int n)
   char buf[BSIZE];
   uint indirect[NINDIRECT];
   uint x;
-
+  
   rinode(inum, &din);
   off = xint(din.size);
   // printf("append inum %d at off %d sz %d\n", inum, off, n);
@@ -322,17 +391,17 @@ iappend(uint inum, void *xp, int n)
     assert(fbn < MAXFILE);
     if(fbn < NDIRECT){
       if(xint(din.addrs[fbn]) == 0){
-        din.addrs[fbn] = xint(freeblock++);
+	din.addrs[fbn] = xint(freeblock++);
       }
       x = xint(din.addrs[fbn]);
     } else {
       if(xint(din.addrs[NDIRECT]) == 0){
-        din.addrs[NDIRECT] = xint(freeblock++);
+	din.addrs[NDIRECT] = xint(freeblock++);
       }
       rsect(xint(din.addrs[NDIRECT]), (char*)indirect);
       if(indirect[fbn - NDIRECT] == 0){
-        indirect[fbn - NDIRECT] = xint(freeblock++);
-        wsect(xint(din.addrs[NDIRECT]), (char*)indirect);
+	indirect[fbn - NDIRECT] = xint(freeblock++);
+	wsect(xint(din.addrs[NDIRECT]), (char*)indirect);
       }
       x = xint(indirect[fbn-NDIRECT]);
     }
